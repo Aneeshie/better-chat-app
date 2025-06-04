e97f678d0019e329ac84aa47d266a3ed5fb7d864
Aneeshie <aneeshdas556@gmail.com>
2025-06-04 11:53:51 +0530

    initial frontend and backend setup

---
+++ backend/controller/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup =  (req,res) => {
+  res.send("This is the signup route");
+};
+
+export const login =  (req,res) => {
+  res.send("This is the login route");
+};
+
+export const logout =  (req,res) => {
+  res.send("This is the logout route");
+};
---
+++ backend/server.js
@@ -0,0 +1,19 @@
+import express from "express"
+import dotenv from "dotenv"
+import authRoutes from "./routes/auth.routes.js"
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/",(req,res) => {
+  res.send("this is the root route");
+});
+
+app.use("/api/auth",authRoutes);
+
+
+app.listen(PORT,() => {
+  console.log(`The server is running on PORT ${PORT}`)
+})
